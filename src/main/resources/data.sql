db7812e2a4aa5b354d09669386386e5b94480405
ALU2K <arzmoevius@gmail.com>
2024-01-29 20:05:32 +0100

    cerramos el intelli

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,16 @@
 INSERT INTO anime(titulo,autor,genero,emision) VALUES (Zipang,Kawaguchi Kaiji,historico,false)
 INSERT INTO anime(titulo,autor,genero,emision) VALUES (One Piece,Eichiro Oda,aventura,true)
-INSERT INTO anime(titulo,autor,genero,emision) VALUES (Martial peak,Momo,artes marciales,true)
 INSERT INTO anime(titulo,autor,genero,emision) VALUES (Hajime no Ippo,MORIKAWA George ,deportes,true)
+INSERT INTO anime(titulo,autor,genero,emision) VALUES (Beastars,Itagaki Paru,psicologico,true)
+INSERT INTO anime(titulo,autor,genero,emision) VALUES (Baki,Keisuke Itagaki,artes marciales,false)
+INSERT INTO anime(titulo,autor,genero,emision) VALUES (Overlord,Kugane Maruyama,isekai,true)
+INSERT INTO anime(titulo,autor,genero,emision) VALUES (Beastars,Itagaki Paru,psicologico,false)
 INSERT INTO anime(titulo,autor,genero,emision) VALUES (Beastars,Itagaki Paru,psicologico,false)
 
 
 
+
+INSERT INTO anime(titulo,autor,genero,emision) VALUES (Martial peak,Momo,artes marciales,true)
 
 
 
